f516ee39c8835a7d1a45220002fbff8097d9c0e4
BawNer <aleksandr.krasnikov2000@gmail.com>
2021-12-23 23:32:07 +0300

    компонент системных уведомлений

---
+++ src/components/system/alert.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+
+    <q-dialog v-model="sys.active" seamless position="bottom">
+      <q-card style="width: 350px">
+        <q-toolbar>
+          <q-toolbar-title>
+            <span class="text-weight-bold">Avem</span> notifications
+          </q-toolbar-title>
+          <q-btn flat round dense icon="close"></q-btn>
+        </q-toolbar>
+        <q-card-section>
+          {{sys.message}}
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default ({
+  name: 'systemAlert',
+  setup() {
+    const store = useStore()
+
+    return {
+      sys: computed(() => store.getters['system/getSystemMessage'])
+    }
+  },
+})
+</script>
