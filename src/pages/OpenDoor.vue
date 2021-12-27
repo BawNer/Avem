7b0d8b173463edeacb557c7a8ea0af6d91643642
BawNer <aleksandr.krasnikov2000@gmail.com>
2021-12-27 21:00:29 +0300

    Новые страницы

---
+++ src/pages/OpenDoor.vue
@@ -0,0 +1,111 @@
+<template>
+  <q-page padding>
+    <div class="row q-mx-xl">
+      <div class="col-12 q-my-xl">
+        <span class="text-h3 text-grey-10">День открытых дверей</span>
+      </div>
+      <div class="col-12 text-subtitle1">
+        Мы уверены, что день открытых дверей позволит Вам сделать лучший выбор траектории своей профессиональной деятельности и связать свой путь с реально современной и динамической отраслью телекоммуникаций, которая ждет молодых и перспективных работников.
+      </div>
+      <div class="col-12 q-my-xl">
+        <div class="row">
+          <div class="col-12 q-mb-xl">
+            <div class="row">
+              <div class="col-6">
+                <q-card flat>
+                  <q-card-section>
+                    <span class="text-h4">Программа Дня открытых дверей:</span>
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-h5 text-primary">01.</span>
+                    Знакомство с университетом
+                    и выпускающими кафедрами
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-h5 text-primary">02.</span>
+                    Встреча с руководством филиала
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-h5 text-primary">03.</span>
+                    Знакомство с условиями приема
+                    в университет
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-h5 text-primary">04.</span>
+                    Экскурсии по лабораториям
+                  </q-card-section>
+                </q-card>
+              </div>
+              <div class="col-6">
+                <q-img
+                  src="images/open_door/1.png"
+                ></q-img>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 q-mb-xl">
+            <div class="row">
+              <div class="col-6">
+                <q-img
+                  src="images/open_door/2.png"
+                ></q-img>
+              </div>
+              <div class="col-6">
+                <q-card flat>
+                  <q-card-section>
+                    <span class="text-h4">Лучший способ знакомства
+  с вузом — День открытых дверей</span>
+                  </q-card-section>
+                  <q-card-section>
+                    Абитуриенты Ростова-на-Дону готовятся к выбору учебного заведения. Как известно, одним из лучших способов знакомства абитуриента с вузом является посещение дня открытых дверей. Северо-Кавказский филиал Московского технического университета связи и информатики приглашает абитуриентов Ростова-на-Дону и Ростовской области, желающих получить высшее образование по очной или заочной формам обучения, на регулярно проводимые Дни открытых дверей.
+                  </q-card-section>
+                </q-card>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 q-mb-xl">
+            <div class="row">
+              <div class="col-6">
+                <q-card flat>
+                  <q-card-section>
+                    <span class="text-h4">Почувствуй себя студентом!</span>
+                  </q-card-section>
+                  <q-card-section>
+                    На Дне открытых дверей абитуриенты получат возможность познакомиться со всеми сферами студенческой жизни в нашем Филиале. Узнают об особенностях научной работы, познакомятся с основными направлениями внеучебной деятельности студентов: творческими мастерскими, командой КВН, спортивными группами и секциями. <br/>
+                    Тысячи абитуриентов сделали свой выбор в пользу СКФ МТУСИ, пойдя по стопам своих родителей. Среди выпускников насчитывается 271 династия, и некоторым из них уже более двух веков.
+                  </q-card-section>
+                </q-card>
+              </div>
+              <div class="col-6">
+                <q-img
+                  src="images/open_door/3.png"
+                ></q-img>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 q-mb-xl">
+            <div class="row">
+              <div class="col-6">
+                <q-img
+                  src="images/open_door/4.png"
+                ></q-img>
+              </div>
+              <div class="col-6">
+                <q-card flat>
+                  <q-card-section>
+                    <span class="text-h4">Свыше 8000 специалистов 
+— выпускники СКФ МТУСИ</span>
+                  </q-card-section>
+                  <q-card-section>
+                    За свою 50-летнюю историю Филиал подготовил свыше 8000 квалифицированных инженеров и экономистов, многие из которых являются руководителями высшего звена предприятий связи Юга России. Все они входят в своеобразный элитный клуб связистов. Теперь и у Вас есть уникальная возможность стать членами этого клуба.
+                  </q-card-section>
+                </q-card>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
