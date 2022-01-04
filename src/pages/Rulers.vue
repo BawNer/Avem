1c31164c0b7aa9afc7ab1e8967c1f72279a41b78
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-01-04 19:21:54 +0300

    Добавлена страница с руководством

---
+++ src/pages/Rulers.vue
@@ -0,0 +1,92 @@
+<template>
+  <q-page padding>
+    <div class="row q-mx-xl">
+      <div class="col-12 q-my-xl">
+        <span class="text-h3 text-grey-10">Руководство</span>
+      </div>
+      <div class="col-12">
+        <div class="row">
+          <div class="col-4 q-px-md">
+            <q-card flat bordered>
+              <q-card-section horizontal>
+                <q-card-section class="col-7">
+                  <q-card-section>
+                    <span class="text-h6">Должность: </span> 
+                    <span class="text-subtitle1">Директор</span>
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-subtitle1">Манин Александр Анатольевич</span>
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-h6">Приемная директора: </span> 
+                    <span class="text-subtitle1">Тел. (863) 310-69-60</span>
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-h6">E-mail: </span> 
+                    <span class="text-subtitle1">mаnin@skf-mtusi.ru</span>
+                  </q-card-section>
+                </q-card-section>
+                <q-img
+                  class="col-5"
+                  src="http://skf-mtusi.ru/images/ruk/Manin.jpg"
+                ></q-img>
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-4 q-px-md">
+            <q-card flat bordered>
+              <q-card-section horizontal>
+                <q-card-section class="col-7">
+                  <q-card-section>
+                    <span class="text-h6">Должность: </span> 
+                    <span class="text-subtitle1">	Заместитель директора по учебно-воспитательной работе</span>
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-subtitle1">Жуковский Александр Георгиевич</span>
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-subtitle1">Тел. (863) 310-69-60</span>
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-h6">E-mail: </span> 
+                    <span class="text-subtitle1">zhukоvskiy@skf-mtusi.ru</span>
+                  </q-card-section>
+                </q-card-section>
+                <q-img
+                  class="col-5"
+                  src="http://www.skf-mtusi.ru/images/ruk/Zhukovskiy.jpg"
+                ></q-img>
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-4 q-px-md">
+            <q-card flat bordered>
+              <q-card-section horizontal>
+                <q-card-section class="col-7">
+                  <q-card-section>
+                    <span class="text-h6">Должность: </span> 
+                    <span class="text-subtitle1">	Главный бухгалтер</span>
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-subtitle1">Демченко Лариса Юрьевна</span>
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-subtitle1">Тел. (863) 307-61-91</span>
+                  </q-card-section>
+                  <q-card-section>
+                    <span class="text-h6">E-mail: </span> 
+                    <span class="text-subtitle1">buhgаlteria@skf-mtusi.ru</span>
+                  </q-card-section>
+                </q-card-section>
+                <q-img
+                  class="col-5"
+                  src="http://skf-mtusi.ru/images/ruk/Osipovich.jpg"
+                ></q-img>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
