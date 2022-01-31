21d875a7f31d3373b08d5d1f37aec5ff716a2604
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-01-31 17:25:30 +0300

    Добавлена страница событий

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -5,67 +5,98 @@
         <span class="text-h3 text-grey-10">События в СКФ МТУСИ</span>
       </div>
       <div class="col-12 q-my-xl">
-        {{evt}}
-        <!-- <div class="row">
+        <div class="row">
           <div class="col-md-4 col-12">
-            <div class="column">
-              <div class="col-12 q-pa-md" v-for="(evt, index) in event" :key="index">
-                <q-card v-if="index % 3 === 0">
-                  <q-card-section>
-                    <q-card-label>{{evt.label}}</q-card-label>
-                  </q-card-section>
-                </q-card>
-              </div>
+            <div class="column q-pa-md">
+              <q-card v-for="(cl, index) in column1" :key="index" class="q-mb-md">
+                <q-card-section>
+                  <span class="text-overline">{{cl.date}}</span>
+                  <div class="text-h6">{{cl.label}}</div>
+                </q-card-section>
+                <q-card-section>
+                  <p>{{cl.description}}</p>
+                </q-card-section>
+                <q-card-actions>
+                  <q-btn class="text-primary" flat>{{cl.timeStart}}-{{cl.timeEnd}}</q-btn>
+                  <q-space></q-space>
+                  <q-btn class="text-uppercase" flat>{{cl.audience}}</q-btn>
+                </q-card-actions>
+              </q-card>
             </div>
           </div>
           <div class="col-md-4 col-12">
-            <div class="column">
-              <div class="col-12 q-pa-md" v-for="(evt, index) in event" :key="index">
-                <q-card v-if="index % 2 === 0">
-                  <q-card-section>
-                    <q-card-label>{{evt.label}}</q-card-label>
-                  </q-card-section>
-                </q-card>
-              </div>
+            <div class="column q-pa-md">
+             <q-card v-for="(cl, index) in column2" :key="index" class="q-mb-md">
+                <q-card-section>
+                  <span class="text-overline">{{cl.date}}</span>
+                  <div class="text-h6">{{cl.label}}</div>
+                </q-card-section>
+                <q-card-section>
+                  <p>{{cl.description}}</p>
+                </q-card-section>
+                <q-card-actions>
+                  <q-btn class="text-primary" flat>{{cl.timeStart}}-{{cl.timeEnd}}</q-btn>
+                  <q-space></q-space>
+                  <q-btn class="text-uppercase" flat>{{cl.audience}}</q-btn>
+                </q-card-actions>
+              </q-card>
             </div>
           </div>
           <div class="col-md-4 col-12">
-            <div class="column">
-              <div class="col-12 q-pa-md" v-for="(evt, index) in event" :key="index">
-                <q-card>
-                  <q-card-section>
-                    <q-card-label>{{evt.label}}</q-card-label>
-                  </q-card-section>
-                </q-card>
-              </div>
+            <div class="column q-pa-md">
+              <q-card v-for="(cl, index) in column3" :key="index" class="q-mb-md">
+                <q-card-section>
+                  <span class="text-overline">{{cl.date}}</span>
+                  <div class="text-h6">{{cl.label}}</div>
+                </q-card-section>
+                <q-card-section>
+                  <p>{{cl.description}}</p>
+                </q-card-section>
+                <q-card-actions>
+                  <q-btn class="text-primary" flat>{{cl.timeStart}}-{{cl.timeEnd}}</q-btn>
+                  <q-space></q-space>
+                  <q-btn class="text-uppercase" flat>{{cl.audience}}</q-btn>
+                </q-card-actions>
+              </q-card>
             </div>
           </div>
-        </div> -->
+        </div>
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import { comuted } from 'vue'
 
 export default {
   setup() {
-    const event = [
-      { label: '1' },
-      { label: '2' },
-      { label: '3' },
-      { label: '4' },
-      { label: '5' },
-      { label: '6' },
-      { label: '7' },
-      { label: '8' },
-      { label: '9' },
+    const evt = [
+      { label: 'Начало учебного семестра', description: 'Начало 2 семестра', date: '08.02.2022', timeStart: '20.00', timeEnd: '21.00', audience: 'Актовый зал, 304' },
+      { label: 'Начало учебного семестра', description: 'Начало 2 семестра', date: '08.02.2022', timeStart: '20.00', timeEnd: '21.00', audience: 'Актовый зал, 304' },
+      { label: 'Начало учебного семестра', description: 'Начало 2 семестра', date: '08.02.2022', timeStart: '20.00', timeEnd: '21.00', audience: 'Актовый зал, 304' },
+      { label: 'Начало учебного семестра', description: 'Начало 2 семестра', date: '08.02.2022', timeStart: '20.00', timeEnd: '21.00', audience: 'Актовый зал, 304' },
+      { label: 'Начало учебного семестра', description: 'Начало 2 семестра', date: '08.02.2022', timeStart: '20.00', timeEnd: '21.00', audience: 'Актовый зал, 304' },
+      { label: 'Начало учебного семестра', description: 'Начало 2 семестра', date: '08.02.2022', timeStart: '20.00', timeEnd: '21.00', audience: 'Актовый зал, 304' },
+      { label: 'Начало учебного семестра', description: 'Начало 2 семестра', date: '08.02.2022', timeStart: '20.00', timeEnd: '21.00', audience: 'Актовый зал, 304' },
+      { label: 'Начало учебного семестра', description: 'Начало 2 семестра', date: '08.02.2022', timeStart: '20.00', timeEnd: '21.00', audience: 'Актовый зал, 304' },
+      { label: 'Начало учебного семестра', description: 'Начало 2 семестра', date: '08.02.2022', timeStart: '20.00', timeEnd: '21.00', audience: 'Актовый зал, 304' },
     ]
 
+    const switchArrayByColumn = (nodeArray, idx = 1) => {
+      const filteredArray = nodeArray.filter((element, index) => ++index % idx === 0)
+      return catFilteredArrayFromNodeArray(nodeArray, filteredArray)
+    }
+
+    const catFilteredArrayFromNodeArray = (nodeArray, targetarray) => Object.keys(targetarray).map(a => nodeArray.splice(nodeArray.indexOf(targetarray[a]), 1).shift())
+
+    const column3 = switchArrayByColumn(evt, 3)
+    const column2 = switchArrayByColumn(evt, 2)
+    const column1 = switchArrayByColumn(evt)
     
     return {
-      evt
+      column3,
+      column2,
+      column1
     }
   },
 }
