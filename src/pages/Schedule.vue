09c8576fffeb084805d43ef31fad44f5cee37b89
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-02-07 16:05:12 +0300

    исправлено отображение расписания

--- src/pages/Schedule.vue
+++ src/pages/Schedule.vue
@@ -17,63 +17,61 @@
       </div>
       <div class="col-12" v-if="groupSchedule">
         <div class="row">
-          <div class="col-3 q-pa-sm" v-for="(schema, index) in groupSchedule.schema" :key="index">
-            <q-card>
-              <q-item>
-                <q-item-section>
-                  <q-item-label class="header">{{new Date(schema.date).toLocaleDateString("ru")}}</q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-card-section>
-                <span class="text-overline">Занятия:</span>
-
-                <q-list v-for="(classEdu, ic) in schema.classes" :key="ic">
-                  <q-item>
-                    <q-item-section avatar>
-                      <q-icon name="mdi-progress-clock" color="positive"></q-icon>
-                    </q-item-section>
-                    <q-item-section>
-                      <q-item-label>Пара: {{classEdu.lesson}}</q-item-label>
-                      <q-item-label caption>{{classEdu.subjectType}}</q-item-label>
-                    </q-item-section>
-                  </q-item>
-                  <q-item>
-                    <q-item-section avatar>
-                      <q-icon name="mdi-home-map-marker" color="primary"></q-icon>
-                    </q-item-section>
-                    <q-item-section>
-                      <q-item-label>Аудитория: {{classEdu.audience}}</q-item-label>
-                    </q-item-section>
-                  </q-item>
-                  <q-item>
-                    <q-item-section avatar>
-                      <q-icon name="mdi-school" color="orange"></q-icon>
-                    </q-item-section>
-                    <q-item-section>
-                      <q-item-label>Преподаватель: {{classEdu.lecturer}}</q-item-label>
-                    </q-item-section>
-                  </q-item>
-                  <q-item>
-                    <q-item-section avatar>
-                      <q-icon name="mdi-book-education" color="negative"></q-icon>
-                    </q-item-section>
-                    <q-item-section>
-                      <q-item-label>Предмет: {{classEdu.subject}}</q-item-label>
-                    </q-item-section>
-                  </q-item>
-                  <q-separator></q-separator>
-                </q-list>
-<!-- 
+          <template v-for="(schema, index) in groupSchedule.schema" :key="index" >
+            <div class="col-md-3 col-12 q-pa-sm" v-if="new Date().getTime() <= new Date(schema.date)">
+              <q-card>
                 <q-item>
+                  <q-item-section>
+                    <q-item-label class="text-body1">{{new Date(schema.date).toLocaleDateString("ru")}}</q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-card-section>
+                  <span class="text-overline">Занятия:</span>
 
-                  <q-item-section>Преподаватель: {{classEdu.lecturer}}</q-item-section>
-                  <q-item-section>Предмет: {{classEdu.subject}}</q-item-section>
-                  <q-item-section>Тип урока: {{classEdu.subjectType}}</q-item-section>
-                </q-item> -->
-              </q-card-section>
-            </q-card>
-          </div>
+                  <q-list v-for="(classEdu, ic) in schema.classes" :key="ic">
+                    <q-item>
+                      <q-item-section avatar>
+                        <q-icon name="mdi-progress-clock" color="positive"></q-icon>
+                      </q-item-section>
+                      <q-item-section>
+                        <q-item-label>Пара: {{classEdu.lesson}}</q-item-label>
+                        <q-item-label caption>{{classEdu.subjectType}}</q-item-label>
+                      </q-item-section>
+                    </q-item>
+                    <q-item>
+                      <q-item-section avatar>
+                        <q-icon name="mdi-home-map-marker" color="primary"></q-icon>
+                      </q-item-section>
+                      <q-item-section>
+                        <q-item-label>Аудитория: {{classEdu.audience}}</q-item-label>
+                      </q-item-section>
+                    </q-item>
+                    <q-item>
+                      <q-item-section avatar>
+                        <q-icon name="mdi-school" color="orange"></q-icon>
+                      </q-item-section>
+                      <q-item-section>
+                        <q-item-label>Преподаватель: {{classEdu.lecturer}}</q-item-label>
+                      </q-item-section>
+                    </q-item>
+                    <q-item>
+                      <q-item-section avatar>
+                        <q-icon name="mdi-book-education" color="negative"></q-icon>
+                      </q-item-section>
+                      <q-item-section>
+                        <q-item-label>Предмет: {{classEdu.subject}}</q-item-label>
+                      </q-item-section>
+                    </q-item>
+                    <q-separator></q-separator>
+                  </q-list>
+                </q-card-section>
+              </q-card>
+            </div>
+          </template>
         </div>
+      </div>
+      <div v-else>
+        <span class="text-h5 text-grey-10">Выберите группу...</span>
       </div>
     </div>
   </q-page>
@@ -88,7 +86,7 @@ export default {
 
     const store = useStore()
 
-    const groupId = ref('ДВ-11')
+    const groupId = ref(null)
 
     const groupSchedule = computed(() => store.getters.getSchedule.filter(schedule => schedule.group === groupId.value).shift())  
 
