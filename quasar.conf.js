f36400772089b027dfc92de7c13bef2866a7f32b
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-03-17 15:20:39 +0300

    support notify plugin

--- quasar.conf.js
+++ quasar.conf.js
@@ -21,7 +21,8 @@ module.exports = configure(function (ctx) {
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
       'axios',
-      'busEvent'
+      'busEvent',
+      'cookie'
     ],
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
@@ -79,7 +80,13 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
-      config: {},
+      config: {
+        notify: {
+          textColor: 'white',
+          textColor: 'white',
+          timeout: 3000
+        }
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -92,7 +99,9 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
