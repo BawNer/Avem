f14a9342f6c6e383721b83d749796fa81cb36506
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-05-26 17:39:43 +0300

    убраны отступы, изменен список

--- src/pages/University/EduStandarts.vue
+++ src/pages/University/EduStandarts.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page padding>
-    <div class="row" :class="!$q.screen.lt.md ? 'q-mx-xl' : ''">
+    <div class="row">
       <div class="col-12 q-my-xl">
         <span class="text-h3 text-grey-10">Образовательные страндарты и требования</span>
       </div>
       <div class="col-12 q-my-xl">
-        <div class="row">
-          <div class="col-lg-auto col-lg-4 col-md-4 col-12 q-pa-md" v-for="(link, index) in links" :key="index">
+        <div class="row q-col-gutter-sm">
+          <div class="col-md-6 col-12" v-for="(link, index) in links" :key="index">
             <q-card>
               <q-item>
                 <q-item-section avatar>
@@ -37,12 +37,12 @@ export default {
   setup() {
     return {
       links: [
-        { label: 'Приказ Министерства науки и высшего образования Российской Федерации от 08.02.2021 № 83 «О внесении изменении в федеральные государственные образовательные стандарты высшего образования — бакалавриат по направлениям подготовки»', path: 'http://www.skf-mtusi.ru/files/info/fgosvo/prikaz_83_08.02.2021.pdf', icon: 'mdi-pdf-box', type: 'Стандарты и требования' },
-        { label: 'Об утверждении аккредитационных показателей по образовательным программам высшего образования', path: 'http://www.skf-mtusi.ru/files/info/fgosvo/prikaz_1094_25.11.2021.pdf', icon: 'mdi-pdf-box', type: 'Стандарты и требования' },
         { label: '09.03.01 Информатика и вычислительная техника', path: 'http://www.skf-mtusi.ru/files/info/fgosvo/090301.pdf', icon: 'mdi-pdf-box', type: 'Направления подготовки' },
         { label: '09.03.01 ФГОС-3++ Информатика и вычислительная техника', path: 'http://www.skf-mtusi.ru/files/info/fgosvo/2017_09_03_01_N_929.pdf', icon: 'mdi-pdf-box', type: 'Направления подготовки' },
         { label: '11.03.02 Инфокоммуникационные технологии и системы связи', path: 'http://www.skf-mtusi.ru/files/info/fgosvo/110302.pdf', icon: 'mdi-pdf-box', type: 'Направления подготовки' },
-        { label: '11.03.02 ФГОС-3++ Инфокоммуникационные технологии и системы связи', path: 'http://www.skf-mtusi.ru/files/info/fgosvo/2017_11_03_02_N_930.pdf', icon: 'mdi-pdf-box', type: 'Направления подготовки' }
+        { label: '11.03.02 ФГОС-3++ Инфокоммуникационные технологии и системы связи', path: 'http://www.skf-mtusi.ru/files/info/fgosvo/2017_11_03_02_N_930.pdf', icon: 'mdi-pdf-box', type: 'Направления подготовки' },
+        { label: 'Приказ Министерства науки и высшего образования Российской Федерации от 08.02.2021 № 83 «О внесении изменении в федеральные государственные образовательные стандарты высшего образования — бакалавриат по направлениям подготовки»', path: 'http://www.skf-mtusi.ru/files/info/fgosvo/prikaz_83_08.02.2021.pdf', icon: 'mdi-pdf-box', type: 'Стандарты и требования' },
+        { label: 'Об утверждении аккредитационных показателей по образовательным программам высшего образования', path: 'http://www.skf-mtusi.ru/files/info/fgosvo/prikaz_1094_25.11.2021.pdf', icon: 'mdi-pdf-box', type: 'Стандарты и требования' }
       ]
     }
   },
